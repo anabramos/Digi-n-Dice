30991d6f98df203dbf72721ba99de42c0f8517f4
anabramos <anaramosbarretto@gmail.com>
2021-09-12 09:58:57 +0000

    Add clicl event listener function to inside DOMContentLoad event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,75 +23,76 @@ document.addEventListener("DOMContentLoaded" , function() {
     setInterval(function() {
         loadRandomQuote(); 
     },3000);
-});
 
-// Wait for the click event to roll for initiative
-let initiativeRoll = document.getElementById("roll-initiative");
-initiativeRoll.addEventListener("click" , function() {
-     
-    let diceRoll = Math.floor(Math.random() * 20) + 1; //returns a random number between 1 - 20
-    let initiativeModifier = document.getElementById("modifier").valueAsNumber; // identify initiative modifier
-    let yourInitiative = diceRoll + initiativeModifier; // sum the random d20 roll with the initiative modifier
-    
-    let initiativeHistory = [];//creates an empty array for the initiative history
+    // Wait for the click event to roll for initiative
+    let initiativeRoll = document.getElementById("roll-initiative");
+    initiativeRoll.addEventListener("click" , function() {
+        
+        let diceRoll = Math.floor(Math.random() * 20) + 1; //returns a random number between 1 - 20
+        let initiativeModifier = document.getElementById("modifier").valueAsNumber; // identify initiative modifier
+        let yourInitiative = diceRoll + initiativeModifier; // sum the random d20 roll with the initiative modifier
+        
+        let initiativeHistory = [];//creates an empty array for the initiative history
 
-        // Display message if user does not add an initiative modifier
-        if (isNaN(initiativeModifier)) {
-            document.getElementById("initiative").innerHTML = "Please add your initiative modifier";
-        } else { 
-            document.getElementById("initiative").innerHTML = yourInitiative; // Display initiative value  
-            document.getElementById("initiative").setAttribute("class", "number-big"); // Style initiative value  
-            document.getElementById("history-box").classList.remove("hidden"); // Display initiative history box  
+            // Display message if user does not add an initiative modifier
+            if (isNaN(initiativeModifier)) {
+                document.getElementById("initiative").innerHTML = "Please add your initiative modifier";
+            } else { 
+                document.getElementById("initiative").innerHTML = yourInitiative; // Display initiative value  
+                document.getElementById("initiative").setAttribute("class", "number-big"); // Style initiative value  
+                document.getElementById("history-box").classList.remove("hidden"); // Display initiative history box  
 
-            // Create a initiative log history
-            let initiatives = yourInitiative + "= d" + diceRoll + "+" + initiativeModifier + ", ";
-            let li = document.createElement("li");
-            let list = document.getElementById("initiative-history-list");
-            list.prepend(li); // add a new list item to the end of the list 
+                // Create a initiative log history
+                let initiatives = yourInitiative + "= d" + diceRoll + "+" + initiativeModifier + ", ";
+                let li = document.createElement("li");
+                let list = document.getElementById("initiative-history-list");
+                list.prepend(li); // add a new list item to the end of the list 
 
-            initiativeHistory.push(initiatives); // add initiative to initiativeHistory array
-            li.textContent += initiativeHistory[initiativeHistory.length - 1];
+                initiativeHistory.push(initiatives); // add initiative to initiativeHistory array
+                li.textContent += initiativeHistory[initiativeHistory.length - 1];
 
-            let listLenght = list.getElementsByTagName("li").length;
-            console.log(listLenght); // Log amount of initiative rolls into console
+                let listLenght = list.getElementsByTagName("li").length;
+                console.log(listLenght); // Log amount of initiative rolls into console
 
-            //Remove the earlier initiative rolls from history after 10 rolls
-            if (listLenght > 7){
-                list.removeChild(list.lastElementChild);
+                //Remove the earlier initiative rolls from history after 10 rolls
+                if (listLenght > 7){
+                    list.removeChild(list.lastElementChild);
+                }
             }
+
+        let gifs = [
+            "assets/images/gifs/-0.gif",
+            "assets/images/gifs/1.gif",
+            "assets/images/gifs/2.gif",
+            "assets/images/gifs/3.gif",
+            "assets/images/gifs/4-5.gif",
+            "assets/images/gifs/4-5.gif" ,
+            "assets/images/gifs/6-7.gif",
+            "assets/images/gifs/6-7.gif" ,
+            "assets/images/gifs/8-9.gif",
+            "assets/images/gifs/8-9.gif" ,
+            "assets/images/gifs/10-11.gif",
+            "assets/images/gifs/10-11.gif",
+            "assets/images/gifs/12-13.gif" ,
+            "assets/images/gifs/12-13.gif" ,
+            "assets/images/gifs/14.gif" ,
+            "assets/images/gifs/15.gif" ,
+            "assets/images/gifs/16.gif" ,
+            "assets/images/gifs/17-18.gif" ,
+            "assets/images/gifs/17-18.gif" ,
+            "assets/images/gifs/19.gif" ,
+            "assets/images/gifs/20.gif"
+        ];
+
+        // Based on your initiative, display the corresponding gif
+        if (yourInitiative <= 0) {
+            document.getElementById("result-gif").setAttribute("src", "assets/images/gifs/-0.gif");
+        } else if (yourInitiative >= 1 && yourInitiative <= 19) {
+            document.getElementById("result-gif").setAttribute("src", gifs[yourInitiative]);
+        } else if (yourInitiative >= 20){
+            document.getElementById("result-gif").setAttribute("src", "assets/images/gifs/20.gif");
         }
 
-    let gifs = [
-        "assets/images/gifs/-0.gif",
-        "assets/images/gifs/1.gif",
-        "assets/images/gifs/2.gif",
-        "assets/images/gifs/3.gif",
-        "assets/images/gifs/4-5.gif",
-        "assets/images/gifs/4-5.gif" ,
-        "assets/images/gifs/6-7.gif",
-        "assets/images/gifs/6-7.gif" ,
-        "assets/images/gifs/8-9.gif",
-        "assets/images/gifs/8-9.gif" ,
-        "assets/images/gifs/10-11.gif",
-        "assets/images/gifs/10-11.gif",
-        "assets/images/gifs/12-13.gif" ,
-        "assets/images/gifs/12-13.gif" ,
-        "assets/images/gifs/14.gif" ,
-        "assets/images/gifs/15.gif" ,
-        "assets/images/gifs/16.gif" ,
-        "assets/images/gifs/17-18.gif" ,
-        "assets/images/gifs/17-18.gif" ,
-        "assets/images/gifs/19.gif" ,
-        "assets/images/gifs/20.gif"
-    ];
-
-    // Based on your initiative, display the corresponding gif
-    if (yourInitiative <= 0) {
-        document.getElementById("result-gif").setAttribute("src", "assets/images/gifs/-0.gif");
-    } else if (yourInitiative >= 1 && yourInitiative <= 19) {
-        document.getElementById("result-gif").setAttribute("src", gifs[yourInitiative]);
-    } else if (yourInitiative >= 20){
-        document.getElementById("result-gif").setAttribute("src", "assets/images/gifs/20.gif");
-    }
-
+    });
 });
+
